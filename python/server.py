8ab8f56951c4ed55d4e06b8ee025af3d9e0eeade
John Espenhahn <john@espenhahn.org>
2023-12-02 21:16:34 -0800

    Cords

--- python/server.py
+++ python/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify, make_response
 
 from search import search
 
@@ -7,7 +7,19 @@ app = Flask(__name__)
 @app.route('/search')
 def route_search():
     query_param = request.args.get('q', '')  # Default to empty string if 'q' not provided
-    return search(query_param)
+    result = search(query_param)
+
+    response = make_response(jsonify(result))
+    
+    # Here we add the CORS headers manually
+    response.headers.add('Access-Control-Allow-Origin', '*')  # Allow all domains
+    # To allow specific domains instead:
+    # response.headers.add('Access-Control-Allow-Origin', 'https://example.com')
+    
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
